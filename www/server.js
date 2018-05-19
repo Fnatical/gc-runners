79c44e477b88b150eded521ede9953b5989ac752
Damon Claverie <damonclaverie@gmail.com>
2018-05-19 10:14:45 +1000

    Massive changes made.  Directory restructure.

---
+++ www/server.js
@@ -0,0 +1,19 @@
+const express    = require('express');
+const bodyParser = require('body-parser');
+const path       = require('path');
+const app        = express();
+const index      = require('./routes/index');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.use('/', index);
+
+app.listen(3000, function(){
+    console.log('Started server listening on port 3000...');
+});
